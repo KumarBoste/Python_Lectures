67bf61b6b3b40dc049ebcd2070399f31214a509f
Kumar Balaram Boste <bostekumar@gmail.com>
2025-11-13 10:30:03 +0530

    Create LEC - 2.py

---
+++ LEC - 2.py
@@ -0,0 +1,59 @@
+# DAY - 2 :
+
+## SET 
+
+a = {1,1,2,2,2,3,3,4,5,5,6}
+print(a)
+print(type(a))
+
+txt = {"USA","USA",12,12,12,10,10,"India"}
+print(txt)
+
+## DICTONARY 
+
+d = {"India" : [1,2,3,4,5],
+     "USA": [10,20,30,40,50],
+     "UK": [45,35,23,15,67]}
+d
+print(type(d))
+
+## MATHMATICAL OPERATORS 
+'''
++
+-
+*
+/ - give output in Float / decimal
+// - give output in Integer
+% - give  remaindor as output
+'''
+
+print(100 * 2 / 2 + 10)
+print(1000 // 2)
+print(1000 / 2)
+print(40 % 3)
+print(20 ** 2)
+print(8 ** 0.5)
+
+## LOGICAL OPERATOR
+
+
+## COMPARISON OPERATOR
+
+print(100 <= 200) # less than or equal to
+print(200 != 300) # Not equal to
+print(1000 == 1000) # comparison ==
+
+## USER DEFINED FUNCTION 
+### Program :  Addition of Two Numbers
+
+'''
+1. Ask user to enter th two numbers.
+2. Addition operation will be performed
+3. Output will get printed.
+'''
+number1 = float(input("Enter the first number:"))
+number2 = float(input("Enter the first number:"))
+
+number3 = number1 + number2 
+
+print(number3)
