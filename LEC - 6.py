4322531c240e58bece99279de7e96b2fa98f5151
Kumar Balaram Boste <bostekumar@gmail.com>
2025-11-20 10:35:29 +0530

    Update LEC - 6.py

--- LEC - 6.py
+++ LEC - 6.py
@@ -9,3 +9,45 @@
 
 a.count(1)
 print(a)
+
+'''
+index : postion of value
+'''
+a.index(1)
+print(a)
+
+'''
+Extend : extend a containner in list.
+'''
+a.extend([10,20,30,40])
+print(a)
+
+# Using Reverse()
+lst =[10,200,3,4000,2,800]
+lst.reverse()
+print(lst)
+
+# Using Sort() - for Number
+lst.sort
+lst
+
+# using sort() - Alphabet
+lst1 = ["Hello","Beautifull","World"]
+lst1.sort
+lst1
+
+# PYTHON FOLLOWS (A-Z , a-z, 0-9)
+
+
+# concept if Indexing and Slicing'
+## lst [index]
+lst = [1,2,3,4,5,6,7,8,9,10]
+print(lst)
+
+# lst[start:stop]
+print(lst[2:6])
+print(lst[1::3])
+print(lst[-1::4])
+print(lst[-1::-2])
+
+
