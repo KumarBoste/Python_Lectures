80b93e1afd6bf32c3948eeeb2a8c77b3dc7a6b54
Kumar Balaram Boste <bostekumar@gmail.com>
2025-11-25 10:20:25 +0530

    Create LEC - 9.py

---
+++ LEC - 9.py
@@ -0,0 +1,89 @@
+'''
+----------------------------------------------
+----------------------------------------------
+# DAY - 9 :
+----------------------------------------------
+----------------------------------------------
+'''
+ 
+# Iterator and Iterable
+
+for i in range(1,10):
+  print(i)  # i is called iterator
+
+
+# Calculate length of each string  
+
+## Define a iterable :  List
+lst = ["Hello","World","PythonProgramming","DataScinece","Java"]
+
+bucket1 = []
+bucket2 = []
+
+for i in lst:
+  if len(i) > 5:
+    bucket1.append(i)
+  else:
+    bucket2.append(i)
+print(bucket1)
+print(bucket2)
+
+
+
+# Calculate length of each string using def function
+
+## Define a iterable :  List
+lst = ["Hello","World","PythonProgramming","DataScinece","Java"]
+
+def compute():  #Function definition
+
+  bucket1 = []
+  bucket2 = []
+
+  for i in lst:
+    if len(i) > 5:
+      bucket1.append(i)
+    else:
+      bucket2.append(i)
+  return bucket1,bucket2
+
+bucket1,bucket2 = compute()  # Function Calling
+print(bucket1)
+print(bucket2)
+
+# Vowels Program
+
+lst1 = ["a","e","h","k","o","u"]
+
+bucket1 = []
+bucket2 = []
+
+for i in lst1:
+  if i in "aeiou":
+    bucket1.append(i)
+  else:
+    bucket2.append(i)
+
+print(bucket1)
+print(bucket2)
+
+
+# Vowels Program using def
+
+lst1 = ["a","e","h","k","o","u"]
+def compute2():
+  bucket1 = []
+  bucket2 = []
+
+  for i in lst1:
+    if i in "aeiou":
+      bucket1.append(i)
+    else:
+      bucket2.append(i)
+  return bucket1,bucket2
+
+bucket1,bucket2 = compute2()
+print(bucket1)
+print(bucket2)
+
+
